ca5705bccb3f80961264ca23d1ac1b921bfe1dc4
Erik Arvstedt <erik.arvstedt@gmail.com>
2023-05-09 17:02:01 +0200

    move update scripts

--- pkgs/mempool/scripts/frontend-assets-update.sh
+++ pkgs/mempool/scripts/frontend-assets-update.sh
@@ -6,9 +6,9 @@ updateRepoHash() {
     echo -n "Fetching latest rev for $repo: "
     hash=$(curl -fsS "https://api.github.com/repos/$repo/commits/master" | jq -r '.sha')
     echo "$hash"
-    sed -i -E "s|( +)\[\"$repo(.*)|\1[\"$repo\"]=$hash|" frontend-assets-fetch.sh
+    sed -i -E "s|( +)\[\"$repo(.*)|\1[\"$repo\"]=$hash|" scripts/frontend-assets-fetch.sh
 }
 
-<frontend-assets-fetch.sh sed -nE 's| +\["([^"]+).*|\1|p' | while read -r repo; do
+<scripts/frontend-assets-fetch.sh sed -nE 's| +\["([^"]+).*|\1|p' | while read -r repo; do
     updateRepoHash "$repo"
 done
